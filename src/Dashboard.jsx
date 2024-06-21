d650178b1396508e1843d8fca800fea12b12e98a
Your Name <you@example.com>
2024-06-21 05:43:26 +0530

    c

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -5,29 +5,29 @@ const Dashboard = () => {
   let nav = useNavigate();
   return (
     <section>
-      <nav className=" h-[5em] w-screen flex items-center gap-72 flex-grow  border-slate-950 ">
+      <nav className="h-[4.8em]  w- flex items-center gap-72 flex-grow  border border-slate-950 ">
         <div className=" w-3/6 h-4/5 flex gap-16 justify-end flex-shrink">
           <div className=" flex items-center ">
             <img src={logo} alt="" className="size-10" />
           </div>
-          <div className="h-full w-1/8 border-black  flex justify-center items-center   border-b-2">
-            <NavLink to="home" onClick={() => nav("home")}>
+          <div className="h-full w-1/8 flex justify-center items-center ">
+            <NavLink to="home" className={'h-8'} onClick={() => nav("home")}>
               Home
             </NavLink>
           </div>
 
-          <div className="h-full w-1/8 border-black  flex justify-center items-center   border-b-2">
-            <NavLink to="gift" onClick={() => nav("gift")}>
+          <div className="h-full w-1/8   flex justify-center items-center  ">
+            <NavLink to="gift" className={'h-8'} onClick={() => nav("gift")}>
               Gift
             </NavLink>
           </div>
-          <div className="h-full w-1/8 border-black  flex justify-center items-center   border-b-2">
-            <NavLink to="pay" onClick={() => nav("pay")}>
+          <div className="h-full w-1/8   flex justify-center items-center   ">
+            <NavLink to="pay" className={'h-8'} onClick={() => nav("pay")}>
               Pay
             </NavLink>
           </div>
-          <div className="h-full w-1/8 border-black  flex justify-center items-center   border-b-2">
-            <NavLink to="store" onClick={() => nav("store")}>
+          <div className="h-full w-1/8   flex justify-center items-center ">
+            <NavLink to="store" className={'h-8'} onClick={() => nav("store")}>
               Store
             </NavLink>
           </div>
@@ -40,6 +40,7 @@ const Dashboard = () => {
           <i className="fa-regular fa-user border-b-black border h-6"></i>
         </div>
       </nav>
+      <div className="h-[5em] bg-green-700 border border-slate-950"></div>
       <Outlet />
     </section>
   );
